ceaf430230b3c6ee9465021b789479f80b98f146
tanya <minsk_tania@mail.ru>
2023-01-03 15:47:06 +0300

    auth guard created

---
+++ src/app/shared/guards/checkAuth.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import { AuthService } from "../services/auth.service";
+ 
+@Injectable({
+    providedIn: 'root'
+})
+  
+export class CheckAuth implements CanActivate{
+    constructor (private router: Router, private authService: AuthService){}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) : Observable<boolean> | boolean{
+        if (!this.authService.isLoggedIn) {
+            // redirect to some view explaining what happened
+            this.router.navigateByUrl('/login');
+             return false;
+        } else {
+            return true;
+        }
+    }
+}
